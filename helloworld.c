67073d96340f84d13e66ac8d4c50c6ffaeebb20d
Anirudh Venkataramanan <anirudh.venkataramanan@intel.com>
2015-06-02 16:19:36 -0700

    removed some stupid changes

--- helloworld.c
+++ helloworld.c
@@ -6,8 +6,6 @@ int main(int argc, char **argv)
     printf("Hello World!\n");
     printf("Hello World!\n");
     printf("Hello World!\n");
-    printf("Hello World!\n");
-    printf("Hello World!\n");
 
     return 0;
 }
